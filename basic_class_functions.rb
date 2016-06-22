544179dd081b12f00c7375a56d25038cc3d026b7
= <=>
2016-06-22 09:48:05 +0530

    Ruby Inheritance
    Added basic functionality of classes and use of class vars and
    class methods

---
+++ basic_class_functions.rb
@@ -0,0 +1,19 @@
+class Computer
+    @@users = {}
+    def initialize(username,password)
+        @username = username
+        @password = password
+        @files = {}
+        @@users[username] = password
+        end
+    def create(filename)
+        time = Time.now
+        @files[filename] = time
+        puts "New file created"
+    end
+    def Computer.get_users
+        @@users
+    end
+end
+
+my_computer = Computer.new("x","x")
